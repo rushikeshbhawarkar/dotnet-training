703bfc96c58f42113abb4be3c8a1272667c1210e
Rushikesh Bhawarkar <rushikeshbhawarkar@gmail.com>
2026-07-07 15:08:18 +0530

    Day2

---
+++ Day_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// int n = 3;
+// int y = 4;
+// bool a = true;
+// bool b = false;
+// bool c = a && b;
+// bool d = a||b;
+
+// Console.WriteLine("n ="+n);
+// Console.WriteLine("y="+y);
+// Console.WriteLine("a="+a);
+// Console.WriteLine("b="+b);
+
+// Console.WriteLine("Assignment n++" + n++);
+// Console.WriteLine("a&b"+ c);
+// Console.WriteLine("a||b"+ d);
+// yes
+
+
+int a =15;
+Console.WriteLine("Score = "+a);
+
+if(a>90){
+Console.WriteLine("Grade = A");
+}else if(a>=80){
+    Console.WriteLine("Grade = B");
+}else if (a >= 70){
+    Console.WriteLine("Grade = C");
+} else if (a >= 60){
+    Console.WriteLine("Grade = D");
+}else{
+    Console.WriteLine("Fail");
+}
